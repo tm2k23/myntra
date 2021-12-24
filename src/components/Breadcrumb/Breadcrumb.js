3c79ab2b3dc4121e9ce82d1f76e3fadefaeffafd
Tejas Mishra <tejasmishra2k18@gmail.com>
2021-12-24 21:46:30 +0530

    Style Breadcrumb

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Breadcrumb.css';
 export default function Breadcrumb() {
+    const breadCrumb=[
+        "Home",
+        "Clothing",
+        "T-Shirts",
+    ]
     return (
-        <div className="breadcrumb-container flex-row " >
-            This area is for breadcrumb, I like roti wali sbji i.e. Pizza
+        <div className="breadcrumb-container flex-row" >
+            {
+                breadCrumb.map( (bread,index) => {
+                    return (
+                        <div className="breadcrumb-item" key={index} >
+                            {bread}
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
