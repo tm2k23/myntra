513b0c74a966986dbe5cf5a5030ffe0a1e816ea0
Tejas Mishra <tejasmishra2k18@gmail.com>
2021-12-29 00:00:05 +0530

    use lottie file for exmpty wishlist

--- src/containers/WishListContainer/WishListContainer.js
+++ src/containers/WishListContainer/WishListContainer.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './WishListContainer.css'
-import {WishListItemCard} from '../../components/index';
+import {WishListItemCard , Empty} from '../../components/index';
 import {useSelector} from 'react-redux';
 export default function WishListContainer() {
     const products = useSelector(state => state.wishlistStore);
     return (
         <div className="wishlist-container flex-row " >
             {
+                products.length == 0 ?
+                <Empty />
+                :
                 products.map((product,index) =>{
                     return (
                         <WishListItemCard item={product} />
