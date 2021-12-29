e3cc67bca45416b0c468fd0f4be2d80f8021db01
Tejas Mishra <tejasmishra2k18@gmail.com>
2021-12-29 11:08:41 +0530

    Dispatch actions from color filter

--- src/components/FilterOptions/Color.js
+++ src/components/FilterOptions/Color.js
@@ -1,8 +1,18 @@
 // this component is to display color filter
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import './Filters.css';
 import {filterColors} from '../../utils/FilterColors';
+import { addColorFilter , removeColorFilter} from '../../actions/filters';
 export default function Color({activeFilter}) {
+    const dispatch = useDispatch();
+    function colorChangeHandler(event){
+        if(event.target.checked){
+            dispatch( addColorFilter( event.target.value))
+        }else{
+            dispatch( removeColorFilter( event.target.value))
+        }
+    }
     return (
         <div className=" specific-filter-container " >
             <div className="filter-title" >COLOR</div>
@@ -13,8 +23,9 @@ export default function Color({activeFilter}) {
                             <input 
                                 type="checkbox" 
                                 name="color" 
-                                value={color}
+                                value={color.name}
                                 checked={activeFilter.includes(color.name)}
+                                onClick={ colorChangeHandler }
                             ></input>
                             <span className="filter-name">
                                 <div className="filter-color-sample" style={{backgroundColor:color.hex }} ></div>
