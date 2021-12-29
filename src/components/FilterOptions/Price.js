738db4570718989e47c5bf6aa801645590523724
Tejas Mishra <tejasmishra2k18@gmail.com>
2021-12-29 11:15:12 +0530

    Dsiaptch actions from price filter

--- src/components/FilterOptions/Price.js
+++ src/components/FilterOptions/Price.js
@@ -1,9 +1,20 @@
 // this component is to display price filter
 import React from 'react';
 import './Filters.css';
+import { useDispatch } from 'react-redux';
 import {filterPrices} from '../../utils/FilterPrices';
 import {checkPriceInFilter} from '../../helpers/selectors';
+import { addPriceFilter , removePriceFilter} from '../../actions/filters';
 export default function Price({activeFilter}) {
+    const dispatch = useDispatch();
+    function priceChangeHandler( price,event ){
+        console.log(event);
+        if(event.target.checked){
+            dispatch( addPriceFilter( price ))
+        }else{
+            dispatch( removePriceFilter( price ))
+        }
+    }
     return (
         <div className=" specific-filter-container " >
             <div className="filter-title" >PRICE</div>
@@ -17,6 +28,7 @@ export default function Price({activeFilter}) {
                                 name="price"
                                 value={price}
                                 checked={checkPriceInFilter(price , activeFilter)}
+                                onClick={(event) =>{priceChangeHandler(price,event)} }
                             ></input>
                             <span className="filter-name">
                                 Rs. {price.start} to Rs. {price.end}
