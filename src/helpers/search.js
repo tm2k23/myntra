9766458eb7b8989b873446af03cef91f536d7899
Tejas Mishra <tejasmishra2k18@gmail.com>
2021-12-30 09:55:07 +0530

    Fix searching bug , change the logic

--- src/helpers/search.js
+++ src/helpers/search.js
@@ -1,19 +1,23 @@
+// this function checks if all the elements of target are present in arr
+let checkIfAllElementsPresent = (arr, target) => target.every(v => arr.includes(v));
+
 export function searcher( products , query ){
     console.log('searching');
     if( query !== null && query!=="" ){
+
+        // split the query
+        let splitedQuery = query.split(" ");
+        for( let i = 0; i < splitedQuery.length; i++ ){
+            splitedQuery[i] = splitedQuery[i].toLowerCase();
+        }
+
         return products.filter(product => {
-            let splitedProductName = product.productName.split(" ");
-            let splitedQuery = query.split(" ");
-            for( let i = 0; i < splitedQuery.length; i++ ){
-                splitedQuery[i] = splitedQuery[i].toLowerCase();
+            var splitedProductInfo = product.productName.split(" ");
+            splitedProductInfo.push(product.brandName.toLowerCase());
+            for( let i = 0; i < splitedProductInfo.length; i++ ){
+                splitedProductInfo[i] = splitedProductInfo[i].toLowerCase();
             }
-            let isFound = false;
-            splitedProductName.forEach(word => {
-                if( splitedQuery.includes(word.toLowerCase()) ){
-                    isFound = true;
-                }
-            });
-            return isFound || product.brandName.toLowerCase() === query.toLowerCase();
+            return checkIfAllElementsPresent(splitedProductInfo, splitedQuery);
         });
     }
     else{
